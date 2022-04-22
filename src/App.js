b2b5f7ca8c9d30e2dacf37a38cfe3538bf20f515
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-04-22 15:02:30 +0530

    makes changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
+import React, { useState } from 'react'
 //import About from "./components/About"
 
 function App() {
+  let [mode, setMode] = useState('light');     //tells whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if(mode==='light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
+      <Navbar title="YouText" aboutText="About YouText" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze"/>
+      <TextForm heading="Enter the text to analyze" mode={mode}/>
       {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,22 +35,22 @@ export default function TextForm(props) {
     //setText=("new text")  //correct way of changing state
   return (
       <>
-       <div className="container">
+       <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#212529':'white', color: props.mode==='dark'?'white':'#042743'}} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
             <button className="btn btn-primary mx-1" onClick={handleOnCopy}>Copy Text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Space</button>
        </div> 
-       <div className="container my-3">
+       <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes read</p>
             <h3>Preview</h3>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter your text to preview it here:)"}</p>
         </div> 
       </>
   )
